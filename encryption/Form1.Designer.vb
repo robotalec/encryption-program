dd383917c3d7f600bd1468082e178953dcffc773
robotalec <robotalec@gmail.com>
2019-04-21 09:22:23 -0500

    Add files via upload

---
+++ encryption/Form1.Designer.vb
@@ -0,0 +1,310 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.enBtn = New System.Windows.Forms.Button()
+        Me.dec = New System.Windows.Forms.Button()
+        Me.filel = New System.Windows.Forms.TextBox()
+        Me.opFile = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.test = New System.Windows.Forms.Label()
+        Me.txtResult = New System.Windows.Forms.RichTextBox()
+        Me.seedBar = New System.Windows.Forms.TrackBar()
+        Me.txtKey = New System.Windows.Forms.TextBox()
+        Me.txtText = New System.Windows.Forms.RichTextBox()
+        Me.flipBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.filel2 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.outBtn = New System.Windows.Forms.Button()
+        Me.exitbtn = New System.Windows.Forms.Button()
+        Me.errorlbl1 = New System.Windows.Forms.Label()
+        CType(Me.seedBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'enBtn
+        '
+        Me.enBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.enBtn.Location = New System.Drawing.Point(122, 500)
+        Me.enBtn.Name = "enBtn"
+        Me.enBtn.Size = New System.Drawing.Size(100, 42)
+        Me.enBtn.TabIndex = 0
+        Me.enBtn.Text = "Encrypt"
+        Me.enBtn.UseVisualStyleBackColor = True
+        '
+        'dec
+        '
+        Me.dec.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.dec.Location = New System.Drawing.Point(334, 500)
+        Me.dec.Name = "dec"
+        Me.dec.Size = New System.Drawing.Size(100, 42)
+        Me.dec.TabIndex = 1
+        Me.dec.Text = "Decrypt "
+        Me.dec.UseVisualStyleBackColor = True
+        '
+        'filel
+        '
+        Me.filel.Location = New System.Drawing.Point(529, 42)
+        Me.filel.Name = "filel"
+        Me.filel.Size = New System.Drawing.Size(261, 20)
+        Me.filel.TabIndex = 2
+        '
+        'opFile
+        '
+        Me.opFile.Location = New System.Drawing.Point(815, 38)
+        Me.opFile.Name = "opFile"
+        Me.opFile.Size = New System.Drawing.Size(75, 23)
+        Me.opFile.TabIndex = 3
+        Me.opFile.Text = "Open File"
+        Me.opFile.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(529, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(121, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Input Location (optional)"
+        '
+        'test
+        '
+        Me.test.AutoSize = True
+        Me.test.Location = New System.Drawing.Point(28, 414)
+        Me.test.Name = "test"
+        Me.test.Size = New System.Drawing.Size(0, 13)
+        Me.test.TabIndex = 7
+        '
+        'txtResult
+        '
+        Me.txtResult.Location = New System.Drawing.Point(84, 303)
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.Size = New System.Drawing.Size(425, 96)
+        Me.txtResult.TabIndex = 8
+        Me.txtResult.Text = ""
+        '
+        'seedBar
+        '
+        Me.seedBar.Location = New System.Drawing.Point(12, 127)
+        Me.seedBar.Minimum = 1
+        Me.seedBar.Name = "seedBar"
+        Me.seedBar.Size = New System.Drawing.Size(658, 45)
+        Me.seedBar.TabIndex = 6
+        Me.seedBar.TickStyle = System.Windows.Forms.TickStyle.Both
+        Me.seedBar.Value = 1
+        '
+        'txtKey
+        '
+        Me.txtKey.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtKey.Location = New System.Drawing.Point(162, 65)
+        Me.txtKey.MaxLength = 12
+        Me.txtKey.Name = "txtKey"
+        Me.txtKey.Size = New System.Drawing.Size(151, 26)
+        Me.txtKey.TabIndex = 9
+        '
+        'txtText
+        '
+        Me.txtText.Location = New System.Drawing.Point(83, 201)
+        Me.txtText.Name = "txtText"
+        Me.txtText.Size = New System.Drawing.Size(425, 96)
+        Me.txtText.TabIndex = 10
+        Me.txtText.Text = ""
+        '
+        'flipBtn
+        '
+        Me.flipBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.flipBtn.Location = New System.Drawing.Point(228, 500)
+        Me.flipBtn.Name = "flipBtn"
+        Me.flipBtn.Size = New System.Drawing.Size(100, 42)
+        Me.flipBtn.TabIndex = 11
+        Me.flipBtn.Text = "Flip Text"
+        Me.flipBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(25, 232)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Input box"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 340)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Output box"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label4.Location = New System.Drawing.Point(12, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(144, 20)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Number Keyphrase"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(28, 108)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Encryption Level"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(19, 163)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(13, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "1"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(647, 164)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(19, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "10"
+        '
+        'filel2
+        '
+        Me.filel2.Location = New System.Drawing.Point(529, 522)
+        Me.filel2.Name = "filel2"
+        Me.filel2.Size = New System.Drawing.Size(261, 20)
+        Me.filel2.TabIndex = 18
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(532, 500)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(129, 13)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "Output Location (optional)"
+        '
+        'outBtn
+        '
+        Me.outBtn.Location = New System.Drawing.Point(815, 522)
+        Me.outBtn.Name = "outBtn"
+        Me.outBtn.Size = New System.Drawing.Size(75, 23)
+        Me.outBtn.TabIndex = 20
+        Me.outBtn.Text = "Open File"
+        Me.outBtn.UseVisualStyleBackColor = True
+        '
+        'exitbtn
+        '
+        Me.exitbtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.exitbtn.Location = New System.Drawing.Point(228, 573)
+        Me.exitbtn.Name = "exitbtn"
+        Me.exitbtn.Size = New System.Drawing.Size(100, 42)
+        Me.exitbtn.TabIndex = 21
+        Me.exitbtn.Text = "Exit"
+        Me.exitbtn.UseVisualStyleBackColor = True
+        '
+        'errorlbl1
+        '
+        Me.errorlbl1.AutoSize = True
+        Me.errorlbl1.ForeColor = System.Drawing.Color.Red
+        Me.errorlbl1.Location = New System.Drawing.Point(334, 77)
+        Me.errorlbl1.Name = "errorlbl1"
+        Me.errorlbl1.Size = New System.Drawing.Size(199, 13)
+        Me.errorlbl1.TabIndex = 22
+        Me.errorlbl1.Text = "Error keyphrase to big setting to max size"
+        Me.errorlbl1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(921, 730)
+        Me.Controls.Add(Me.errorlbl1)
+        Me.Controls.Add(Me.exitbtn)
+        Me.Controls.Add(Me.outBtn)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.filel2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.flipBtn)
+        Me.Controls.Add(Me.txtText)
+        Me.Controls.Add(Me.txtKey)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.test)
+        Me.Controls.Add(Me.seedBar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.opFile)
+        Me.Controls.Add(Me.filel)
+        Me.Controls.Add(Me.dec)
+        Me.Controls.Add(Me.enBtn)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Encryption System"
+        CType(Me.seedBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents enBtn As Button
+    Friend WithEvents dec As Button
+    Friend WithEvents filel As TextBox
+    Friend WithEvents opFile As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents test As Label
+    Friend WithEvents txtResult As RichTextBox
+    Friend WithEvents seedBar As TrackBar
+    Friend WithEvents txtKey As TextBox
+    Friend WithEvents txtText As RichTextBox
+    Friend WithEvents flipBtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents filel2 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents outBtn As Button
+    Friend WithEvents exitbtn As Button
+    Friend WithEvents errorlbl1 As Label
+End Class
---
+++ encryption/Form1.vb
@@ -0,0 +1,278 @@
+﻿Public Class Form1
+    Dim strOGalphabeht() As Char = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+    Dim strMianCypher() As Char = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+
+    Dim seedInta As Integer
+
+
+    Function EncryptDecrypt(ByVal text1 As String, ByVal key As String, ByVal isEncrypt As Boolean) As String
+        Dim char1 As String 'first character to use
+        Dim char2 As String ' holds the encyrpted or decrypted text
+        Dim cKey As Byte 'holds onto each key number for the program to use
+        Dim strLength As Integer 'length of string to use
+        Dim Result As String = "" 'end result
+        Dim j As Integer = -1 ' J keeps track of where we are in the string so can be assembled in the right order
+
+        If text1 = "" And Not IsNumeric(key) Then
+            text1 = 24
+        End If
+        If text1 <> "" And IsNumeric(key) Then 'controls if the program should run
+                strLength = text1.Length
+                For i As Integer = 0 To strLength - 1
+                    char1 = text1.Substring(i, 1)
+                    If j <key.Length - 1 Then
+                        j= j + 1
+                    Else
+                        j= 0
+                    End If
+                    Console.WriteLine(char1)
+                    Console.WriteLine(char2)
+                    Console.WriteLine(cKey)
+
+                    cKey = Val(key.Substring(j, 1)) 'Takes a number from the key then uses it to determin how far to move on the ASCII board
+                    If isEncrypt Then
+                        char2 = Chr(Asc(char1) + cKey)
+                        Console.Write(char2)
+
+                    Else
+                        char2 = Chr(Asc(char1) - cKey)
+                        Console.Write(char2)
+
+                    End If
+                    Console.Write(char1)
+                    Console.Write(char2)
+
+                    Result &= char2
+                Next
+
+        End If
+            Console.Write(char1)
+            Return Result
+        errorlbl1.Visible = False
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles enBtn.Click
+        Dim stHolder As String
+        stHolder = txtText.Text
+        Dim inHolder As Int64 = Convert.ToInt64(txtKey.Text)
+        If inHolder > 888888888888 Then
+            txtKey.Text = "888888888888"
+            errorlbl1.Visible = True
+        Else
+            errorlbl1.Visible = False
+        End If
+        txtResult.Text = EncryptDecrypt(txtText.Text, txtKey.Text, True)
+        If seedBar.Value > 1 Then
+            For l = 1 To seedBar.Value - 1
+                txtText.Text = txtResult.Text
+                txtResult.Text = EncryptDecrypt(txtText.Text, txtKey.Text, True)
+            Next
+            'ElseIf seedBar.Value <= 1 Then
+            '    txtResult.Text = EncryptDecrypt(txtText.Text, txtKey.Text, True)
+
+        End If
+        txtText.Text = stHolder
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles dec.Click
+        Dim stHolder As String
+        stHolder = txtText.Text
+        Dim inHolder As Int64 = Convert.ToInt64(txtKey.Text)
+        If inHolder > 888888888888 Then
+            txtKey.Text = "888888888888"
+            errorlbl1.Visible = True
+        Else
+            errorlbl1.Visible = False
+
+        End If
+        txtResult.Text = EncryptDecrypt(txtText.Text, txtKey.Text, False)
+        If seedBar.Value > 1 Then
+            For l = 1 To seedBar.Value - 1
+                txtText.Text = txtResult.Text
+
+                txtResult.Text = EncryptDecrypt(txtText.Text, txtKey.Text, False)
+            Next
+        End If
+
+        txtText.Text = stHolder
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub opFile_Click(sender As Object, e As EventArgs) Handles opFile.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+        Dim strFileName As String = "nothing"
+
+        fd.Title = "Open File Dialog"
+        fd.InitialDirectory = "C:\"
+        fd.Filter = "All files (*.*)|*.*|All files (*.*)|*.*"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            strFileName = fd.FileName
+            filel.Text = strFileName
+        End If
+
+        Dim fileReader As System.IO.StreamReader
+        fileReader = My.Computer.FileSystem.OpenTextFileReader(strFileName)
+        Dim stringReader As String
+        stringReader = fileReader.ReadLine()
+        txtText.Text = stringReader
+
+        'Dim fileReader As String
+        'fileReader = My.Computer.FileSystem.ReadAllText(strFileName)
+        'inText.Text = fileReader
+
+    End Sub
+
+
+    Private Sub seedBar_Scroll(sender As Object, e As EventArgs) Handles seedBar.Scroll
+
+        If seedBar.Value > 1 Then
+
+        End If
+
+
+        'seedInta = seedBar.Value
+        Console.WriteLine(seedBar.Value)
+
+        'test.Text = seedInta
+
+        'Dim seedintb As Integer
+
+
+        'Console.WriteLine(temSB.Length)
+        'If seedInta > seedintb Then
+
+        'Dim temSA As Char = strMianCypher(0)
+        'Dim temSB(strMianCypher.Length - 2) As Char
+        'For i = 0 To seedInta
+
+
+        'For iii As Integer = 1 To strMianCypher.Length - 1
+        'temSB(iii - 1) = strMianCypher(iii)
+        'strMianCypher(iii - 1) = temSB(iii - 1)
+
+        'Next iii
+        'strMianCypher(strMianCypher.Length - 1) = temSA
+        'Next i
+        ' ElseIf seedInta < seedintb Then
+        'Dim temSA As Char = strMianCypher(0)
+        'Dim temSB(strMianCypher.Length - 2) As Char
+        ' For i = 0 To seedInta
+        '
+        '
+        ' For iii As Integer = 1 To strMianCypher.Length - 1
+        'temSB(iii + 1) = strMianCypher(iii)
+        'strMianCypher(iii + 1) = temSB(iii + 1)
+
+        'Next iii
+        'strMianCypher(strMianCypher.Length - 1) = temSA
+        ' Next i
+        '
+        ' End If
+
+        'seedintb = seedInta
+
+        ' Console.WriteLine(strMianCypher(0))
+
+        'If seedInta = 0 Then
+        '    strMianCypher = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+        'ElseIf seedInta = 1 Then
+        '    strMianCypher = {"B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A"}
+        'ElseIf seedInta = 2 Then
+        '    strMianCypher = {"C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B"}
+        'ElseIf seedInta = 3 Then
+        '    strMianCypher = {"D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C"}
+        'ElseIf seedInta = 4 Then
+        '    strMianCypher = {"E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D"}
+        'ElseIf seedInta = 5 Then
+        '    strMianCypher = {"F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E"}
+        'ElseIf seedInta = 6 Then
+        '    strMianCypher = {"G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F"}
+        'ElseIf seedInta = 7 Then
+        '    strMianCypher = {"H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G"}
+        'ElseIf seedInta = 8 Then
+        '    strMianCypher = {"I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H"}
+        'ElseIf seedInta = 9 Then
+        '    strMianCypher = {"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I"}
+        'ElseIf seedInta = 10 Then
+        '    strMianCypher = {"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
+        'ElseIf seedInta = 11 Then
+        '    strMianCypher = {"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"}
+        'ElseIf seedInta = 12 Then
+        '    strMianCypher = {"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"}
+        'ElseIf seedInta = 13 Then
+        '    strMianCypher = {"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"}
+        'ElseIf seedInta = 14 Then
+        '    strMianCypher = {"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"}
+        'ElseIf seedInta = 15 Then
+        '    strMianCypher = {"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"}
+        'ElseIf seedInta = 16 Then
+        '    strMianCypher = {"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"}
+        'ElseIf seedInta = 17 Then
+        '    strMianCypher = {"R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q"}
+        'ElseIf seedInta = 18 Then
+        '    strMianCypher = {"S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"}
+        'ElseIf seedInta = 19 Then
+        '    strMianCypher = {"T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"}
+        'ElseIf seedInta = 20 Then
+        '    strMianCypher = {"U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"}
+        'ElseIf seedInta = 21 Then
+        '    strMianCypher = {"V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U"}
+        'ElseIf seedInta = 22 Then
+        '    strMianCypher = {"W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"}
+        'ElseIf seedinta = 23 Then
+        '    strMianCypher = {"X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"}
+        'ElseIf seedInta = 24 Then
+        '    strMianCypher = {"Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"}
+        'ElseIf seedInta = 25 Then
+        '    strMianCypher = {"Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"}
+        'End If
+        'Console.Write(strMianCypher)
+    End Sub
+
+    Private Sub flipBtn_Click(sender As Object, e As EventArgs) Handles flipBtn.Click
+        txtText.Text = txtResult.Text
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub outBtn_Click(sender As Object, e As EventArgs) Handles outBtn.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+        Dim strFileName As String = "nothing"
+
+        fd.Title = "Open File Dialog"
+        fd.InitialDirectory = "C:\"
+        fd.Filter = "All files (*.*)|*.*|All files (*.*)|*.*"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            strFileName = fd.FileName
+            filel2.Text = strFileName
+        End If
+
+        Dim fileReader As System.IO.StreamWriter
+        fileReader = My.Computer.FileSystem.OpenTextFileWriter(strFileName, True)
+        fileReader.WriteLine(txtResult.Text)
+        fileReader.Close()
+    End Sub
+
+    Private Sub exitbtn_Click(sender As Object, e As EventArgs) Handles exitbtn.Click
+        Application.Exit()
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+End Class
